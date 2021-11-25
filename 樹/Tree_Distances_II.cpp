1e5f44a212c543b4b058bf941039437867fcf475
mooky12345 <78418222+mooky12345@users.noreply.github.com>
2021-11-25 21:53:27 +0800

    update

---
+++ 樹/Tree_Distances_II.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define ll long long int
+#define inf 99999999
+#define pb push_back
+#define AC ios::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+
+
+
+vector<int> e[200005];
+int cnt[200005];
+ll ans[200005];
+ll sum = 0;
+int a;
+void dfs(int v,int pre,int dep){
+	cnt[v] = 1;
+	sum += dep;
+	for(int i:e[v]){
+		if(i!=pre){
+			dfs(i,v,dep+1);
+			cnt[v] += cnt[i];
+		}
+	}
+}
+void dfs2(ll summ,int v,int pre){
+	if(v!=pre){
+		summ -= 1LL*2*cnt[v]-a;
+		ans[v] =  summ;
+	}
+	
+	for(int i:e[v]){
+		if(i!=pre){
+			dfs2(summ,i,v);
+		}
+	}
+}
+int main(){
+	AC
+	
+	cin>>a;
+	
+	for(int i=0;i<a-1;i++){
+		int u,v;cin>>u>>v;
+		e[u].pb(v);
+		e[v].pb(u);
+	}
+	dfs(1,1,0);
+	ans[1] = sum; 
+	dfs2(sum,1,1);
+	for(int i=1;i<=a;i++)cout<<ans[i]<<" ";
+}
